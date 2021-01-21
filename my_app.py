f5cf3e2d96f7548d81e39a833fe583ea6a88eb4f
m.mukhlis <m.mukhlis95@gmail.com>
2021-01-21 14:28:33 +0800

    add: my_app.py, routes.py, and all templates

---
+++ my_app.py
@@ -0,0 +1,112 @@
+from flask import Flask
+from flask_mail import Mail, Message
+import sqlite3 as sql
+from datetime import datetime, timedelta
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+app = Flask(__name__)
+mail = Mail(app)
+con = sql.connect('email_database.db')
+
+# set your email (sender) in here:
+your_email = 'your_gmail@gmail.com'
+your_password = 'your_password'
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com' #if your email is not gmail, set using your own mail server
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = your_email
+app.config['MAIL_PASSWORD'] = your_password
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+
+
+def email():
+    """
+    Function for constantly checking time and data from database.
+    The constantly checking can be fulfill with the library BackgroundScheduler from APScheduler.
+    This function will be called from task BackgroundScheduler.
+    If there is/are email(s) that needed to be sent, the function will call function send_email with event_id as the param.
+    """
+    try:
+        """
+        You have to setting the timedelta same with your server time
+        This app assume using UTC+8
+        """
+        utc_8 = datetime.utcnow() + timedelta(hours=8)
+        utc_8 = datetime.timestamp(utc_8)
+        con = sql.connect('email_database.db')
+        c = con.cursor()
+        query = "Select event_id, timestamp FROM save_emails where is_sent='False'"
+        c.execute(query)
+        data = c.fetchall()
+        for el in data:
+            event_id = el[0]
+            time_sent = el[1]
+            schedule = datetime.fromisoformat(time_sent)
+            schedule = datetime.timestamp(schedule)
+            check_time = int(schedule) - (utc_8)
+            if check_time < 0:
+                query = "update save_emails set is_sent=? where event_id=?"
+                temporary = ('True', event_id)
+                c.execute(query, temporary)
+                con.commit()
+            elif 0 <= check_time < 30:
+                query = "update save_emails set is_sent=? where event_id=?"
+                temporary = ('True', event_id)
+                c.execute(query, temporary)
+                con.commit()
+                return send_email(event_id)
+            else:
+                pass
+    except:
+        pass
+    finally:
+        con.close()
+
+def send_app_context(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(event_id):
+    """
+    Function for sending email that to be triggered.
+    This function will call function send_app_context for sending email asynchronously as background task.
+    """
+    try:
+        con = sql.connect('email_database.db')
+        c = con.cursor()
+        c.execute("Select recipients, email_subject, email_content FROM save_emails where event_id=?", (event_id,))
+        data = c.fetchone()
+        recipients = data[0]
+        recipients = list(recipients.split(","))
+        subject = data[1]
+        content = data[2]
+        for user in recipients:
+            msg = Message(subject, sender=your_email, recipients=[user])
+            msg.body = content
+            send_app_context(app, msg)
+    except:
+        pass
+    finally:
+        con.close()
+
+
+
+"""
+This task will running as background task for checking time and send the necessary email
+This task will looping every 30s and will calling function email
+"""
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(email,'interval',seconds=30)
+sched.start()
+
+
+# import files
+from routes import *
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False)
---
+++ routes.py
@@ -0,0 +1,42 @@
+import sqlite3 as sql
+from my_app import app
+from flask import render_template, request
+
+# connect to email_database.sq (database will be created, if not exist)
+con = sql.connect('email_database.db')
+con.execute('CREATE TABLE IF NOT EXISTS save_emails (event_id INTEGER PRIMARY KEY AUTOINCREMENT,'
+            + 'recipients TEXT, email_subject TEXT, email_content TEXT, timestamp TEXT, is_sent TEXT DEFAULT False)')
+con.close()
+
+
+# home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/save_emails', methods=['GET', 'POST'])
+def save_emails():
+    if request.method == 'GET':
+        # send the form
+        return render_template('save_emails.html')
+    else: # request.method == 'POST':
+        # read data from the form and save in variable
+        recipients = request.form['recipients']
+        email_subject = request.form['email_subject']
+        email_content = request.form['email_content']
+        timestamp = request.form['timestamp']
+
+        # store in database
+        try:
+            con = sql.connect('email_database.db')
+            c = con.cursor()
+            c.execute("INSERT INTO save_emails (recipients, email_subject, email_content, timestamp) VALUES (?,?,?,?)",
+                (recipients, email_subject, email_content, timestamp))
+            con.commit()
+            return render_template('createThanks.html')
+        except con.Error as err:
+            # then display the error in 'database_error.html' page
+            return render_template('database_error.html', error=err)
+        finally:
+            con.close()
